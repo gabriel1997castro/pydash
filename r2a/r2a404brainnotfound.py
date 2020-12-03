8c1e87674fc7a4c4d0961fdffbad94b20a17b498
Gabriel Castro <gabriel.castro@coreconsulting.com.br>
2020-12-03 17:19:48 -0300

    Usando tempo de resposta e bps como parâmetros no controlador

--- r2a/r2a404brainnotfound.py
+++ r2a/r2a404brainnotfound.py
@@ -1,19 +1,39 @@
 from player.parser import *
 from r2a.ir2a import IR2A
 import random
+import datetime
+
 
 def normalize(min, max, x):
-  return (x - min)/(max - min)
+    return (x - min)/(max - min)
+
 
 def normalize_negative(min, max, x):
-  return(2*(x - min)/(max - min) -1)
+    return(2*(x - min)/(max - min) - 1)
+
+
+def avg(my_list):
+    return sum(my_list)/len(my_list)
+
 
 class R2A404BrainNotFound(IR2A):
-
     def __init__(self, id):
         IR2A.__init__(self, id)
         self.parsed_mpd = ''
         self.qi = []
+        self.time_of_request = []
+        self.bit_len = []
+        self.connection_speed = []
+        self.quality_id = []
+
+    def add_q_id(self, q_id):
+        self.quality_id += [q_id]
+
+    def add_time(self, time_to_add):
+        self.time_of_request += [time_to_add]
+
+    def add_bit_len(self, bit_to_add):
+        self.bit_len += [bit_to_add]
 
     def handle_xml_request(self, msg):
         self.send_down(msg)
@@ -26,38 +46,64 @@ class R2A404BrainNotFound(IR2A):
         self.send_up(msg)
 
     def handle_segment_size_request(self, msg):
+        moving_avarage_factor = 4
+        datetime.datetime.now()
         buffer = self.whiteboard.get_playback_buffer_size()
+        speed = 0
         if len(buffer) > 0:
             buffer = buffer[-1][1]
+            time_stop = datetime.datetime.now()
+            self.add_time(time_stop.timestamp())
+            if len(self.time_of_request) > 2:
+                time_to_request = self.time_of_request[-1] - self.time_of_request[-2]
+                speed = self.bit_len[-1] / \
+                    (time_to_request)
         else:
             buffer = 0
-        quality = self.whiteboard.get_playback_qi()
-        if len(quality) > 0:
-            quality = quality[-1][1]
+        qualityArr = self.whiteboard.get_playback_qi()
+        if len(qualityArr) > 0:
+            qualityArr = qualityArr[-moving_avarage_factor:]
+            qualityArr = [item[1] for item in qualityArr]
+            quality = avg(qualityArr)
         else:
             quality = 0
         score = 0
-        quality_rate = 1
-        buffer_rate = 1
-        score_rate = 10
+        quality_rate = 1.2
+        buffer_rate = 0.8
+        speed_rate = 2
+        time_rate = 2
+        score_rate = 4
         q = quality
         if quality == 0:
             q = 0.1
-        score += quality_rate * normalize(0, 19, 1/q)
+        score += time_rate * quality_rate * normalize(0, 19, 1/q)
+        if len(self.connection_speed) > 2:
+            score += 1/normalize_negative(0, 1.8, time_to_request)
+            score += speed_rate * \
+                normalize_negative(min(self.connection_speed),
+                                   max(self.connection_speed), speed)
         # score -= 3
-        score += buffer_rate * normalize_negative(0, 60, buffer)
-        print('-----', score_rate * score)
+        score += buffer_rate * normalize_negative(0, 30, buffer)
 
-        quality += score_rate * score
-        if quality > 19:
-            quality = 19
-        if quality < 0:
-            quality = 0
-        
-        msg.add_quality_id(self.qi[int(quality)])
+
+        score_total = score_rate * score
+        if len(qualityArr) > 2:
+            print('quality arr', qualityArr)
+            new_quality = avg(qualityArr+[score_total])
+        else:
+            new_quality = score_total
+        print('new quality', new_quality)
+        if new_quality > 19:
+            new_quality = 19
+        if new_quality < 0:
+            new_quality = 0
+
+        msg.add_quality_id(self.qi[int(new_quality)])
         self.send_down(msg)
 
     def handle_segment_size_response(self, msg):
+        bit_length = msg.get_bit_length()
+        self.add_bit_len(bit_length)
         self.send_up(msg)
 
     def initialize(self):
